0f007e28d96bbd2748c029a0c4bb191e15969c75
Davide Tedesco <davide@MacBook-Pro.local>
2017-07-03 12:18:48 +0200

    aggiunto primo esempio di variazione

---
+++ A.A.-2016-2017/20170703/melodia_fasce.rb
@@ -0,0 +1,18 @@
+require "nota_melodia_lezione"
+
+class MelodiaFasce < Melodia #camel casing
+  N_NOTE=10
+  def variate() #variate crea 10 note che stanno tutte intorno alla nota presa
+    temp = []
+    self.each do
+      |n|
+      1.upto(N_NOTE) do
+        delta = rand()* 0.4 + 0.8
+        nn = Nota.new(n.at,n.lgt,n.vol-20.5,n.ptch*delta,n.tmbr)
+        temp << nn
+      end
+    end
+    self.concat temp
+    self.sort {|a,b| a.at <=> b.at} #sorting delle note sul campo dell'at
+  end
+end
---
+++ A.A.-2016-2017/20170703/test_melodia_fasce.rb
@@ -0,0 +1,5 @@
+require "melodia_fasce"
+
+puts "f1 0 8192 10 1"
+
+m=MelodiaFasce.create_melody("brahms_s4.mel") #creo un'altra classe con la variazione con fasce
